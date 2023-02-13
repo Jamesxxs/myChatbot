2ccf9d2c802308bc9f4c942b15305172ca2ed61d
Jamesxxs <2656020337@qq.com>
2023-02-13 14:19:26 +0800

    Update Mychatbot.py
    Deal with the data

--- Mychatbot.py
+++ Mychatbot.py
@@ -89,4 +89,100 @@ def plot_wordcloud(content,file_prefix =None):
     plt.imshow(w,interpolation='catrom')
     plt.axis('off')
     plt.show()
+STOPWORDS_LIST = stopwords.words('english')
+
+
+questions = df_question['question'].map(lambda x: remove_stopwords(
+                                                    extract_english(
+                                                    remove_punctuation(x)
+                                                    ),
+                                                    stopwords=STOPWORDS_LIST))
+questions = ' '.join(questions)
+word_frequency(questions.split(' '))
+plot_wordcloud(questions)
+
+# %% [markdown]
+# Now, let's make this dataset look similar to our original one, it is: two columns with the same headings
+
+# %%
+# delete columns other than question and nq_answer
+df_question = df_question.drop(
+            columns=['viewed_doc_titles', 'used_queries', 'annotations', 'id', 'nq_doc_title']
+)
+df_question = df_question.reindex(columns=['question', 'nq_answer'])  # swap the order for questions to be first
+df_question = df_question.rename(
+    columns={'question': 'Context', 'nq_answer': 'Text Response'}
+)
+print(df_question.head())
+
+# %%
+# there are brackets included in the Text Responses
+def remove_brackets(text):
+    new_text = str(text).replace('[', '')  # replace left square bracket character with nothing
+    new_text = str(new_text).replace(']', '')  # do the same with the right one
+    return new_text
+
+df_question['Text Response'] = df_question['Text Response'].apply(remove_brackets)  # remove all the brackets for the selected column
+df_question.head()
+
+# %%
+# now we have some apostrophes at the start and end of our responses...
+def remove_first_and_last_character(text):
+    return str(text)[1:-1]  # slice from the second character till the last one (excluding the last one)
+
+df_question['Text Response'] = df_question['Text Response'].apply(remove_first_and_last_character)  # execute it for the whole column
+df_question.head()
+
+# %%
+# compare the number of rows for each dataset
+print("Number of rows in the original dataset: ", df_context.shape[0])
+print("Number of rows in the new dataset: ", df_question.shape[0])
+
+# %%
+df = pd.DataFrame()  # create blank dataframe'
+column1 = [*df_context['Context'].tolist(), *df_question['Context'].tolist()]  # make a list out of the first columns of both datasets
+column2 = [*df_context['Text Response'].tolist(), * df_question['Text Response'].tolist()]  # make a second column by combining both
+df.insert(0, 'Context', column1, True)  # insert first column
+df.insert(1, 'Text Response', column2, True)  # insert second one
+print("Number of rows in the combined dataset: ", df.shape[0])
+
+# %%
+df.head() 
+
+# %% [markdown]
+# Null values are present for the same type of questions whose response can be almost similar and in that similar group of questions, the response is given to the first and the rest filled with null. So, what we can do is use `ffill()` which returns the value of previous response in place of null values as below.
+
+# %%
+df.ffill(axis = 0, inplace = True)   # fill the null value with the previous value
+df.head()  # see first 5 lines
+
+# %% [markdown]
+# # Preprocess data
+
+# %% [markdown]
+# ### Convert text into lower cases and remove special characters and numbers
+
+# %%
+def cleaning(x):
+    cleaned_array = list()
+    for i in x:
+        # convert to all lower letters
+        a = str(i).lower()  
+        
+        # remove any special characters but keep numbers
+        p = re.sub(r'[^a-z0-9]', ' ', a)  
+        
+        # add variable p to our array names cleaned_array
+        cleaned_array.append(p)  
+    return cleaned_array
+
+
+# Create extra column in our dataset just for fun and to see how the cleaned text looks like 
+# in comparison with the original
+df.insert(1, 'Cleaned Context', cleaning(df['Context']), True)
+# first argument indicates position we want this column to be slotted in, second is the name of the new column,
+# third is the array we want to you to fill the rows with and 
+# the last boolean indicates whether to allow duplicates
+df.head()
+
 
